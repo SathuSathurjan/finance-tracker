674eb8cacfc62040ff15dd2fe0df9ff2d6bdc0ca
SathuSathurjan <sathusathurjan2001@gmail.com>
2025-09-09 00:04:26 +0530

    auth gate created

---
+++ lib/widgets/auth_gate.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:personal_finance_tracker/screens/dashboard_screen.dart';
+import 'package:personal_finance_tracker/screens/login_screen.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return LoginScreen();
+        }
+        return DashboardScreen();
+      },
+    );
+  }
+}
